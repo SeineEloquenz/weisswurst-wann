a6fe51736d85f43582a0e75b2aaba58816028067
Alexander Linder <alexander-linder@gmx.de>
2019-04-13 22:59:14 +0200

    Add main activity to application

--- mobile/src/main/java/de/seine_eloquenz/weisswurstwann/MainActivity.java
+++ mobile/src/main/java/de/seine_eloquenz/weisswurstwann/MainActivity.java
@@ -1,12 +1,59 @@
 package de.seine_eloquenz.weisswurstwann;
 
 import android.app.Activity;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
+/**
+ * The Main Activity of weisswurst wann
+ */
 public class MainActivity extends Activity {
+
+    private static boolean wwStatus;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.mainactivity);
+        Button button = (Button) findViewById(R.id.wwbutton);
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                updateInfo();
+            }
+        });
+    }
 
     @Override
     public void onResume() {
         super.onResume();
-        Scheduler.scheduleAlarm(this); //TODO make a real activity, for testing only atm
+        updateInfo();
+    }
+
+    private void updateInfo() {
+        CheckWeisswurstTask task = new CheckWeisswurstTask();
+        TextView text = findViewById(R.id.wwtext);
+        task.execute();
+        if (wwStatus) {
+            text.setText(R.string.wwavailable);
+        } else {
+            text.setText(R.string.wwnotavailable);
+        }
+    }
+
+    private static class CheckWeisswurstTask extends AsyncTask<String, Integer, Boolean> {
+
+        @Override
+        protected Boolean doInBackground(final String... strings) {
+            WeisswurstInfo weisswurstInfo = new WeisswurstInfo();
+            return weisswurstInfo.checkWeisswurstStatus();
+        }
+
+        @Override
+        protected void onPostExecute(Boolean result) {
+            wwStatus = result;
+        }
     }
 }
